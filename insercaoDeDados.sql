55d7f3bf12518fc0ccb5296e5c794be1a422f2a8
Tarcio Teles da Silva Farias <tarcioapps7@gmail.com>
2023-12-30 23:54:54 -0300

    add

---
+++ insercaoDeDados.sql
@@ -0,0 +1,45 @@
+create table cliente(
+	/*
+	id do cliente será um numero inteiro e todo cliente precisa ter id
+	not null = campo não nulo
+	*/
+	idcliente integer not null,
+	nome varchar (50) not null, -- Pedro 5, 45 espaços vazios
+	cpf char (11),
+	rg varchar (15),
+	data_nascimento date,
+	genero char (1),
+	profissao varchar (30),
+	nacionalidade varchar (30),
+	logradouro varchar (30),
+	numero varchar (10),
+	complemento varchar(30),
+	bairro varchar(30),
+	municipio varchar(30),
+	uf varchar(30),
+	Observacoes text,
+	
+	-- primary key
+	constraint pk_cln_idcliente primary key (idcliente)
+)
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (1, 'Tarcio Teles', 42522206508, '485271250', '1994-11-13', 'M', 'Professor', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (2, 'Maria Teles', 42522206508, '485271250', '1994-11-13', 'M', 'Professor', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (3, 'Joana Teles', 42520650845, '485271250', '1994-11-13', 'f', 'cozinheira', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (4, 'José Santos', 42522206508, '485271250', '1994-11-13', 'M', 'Professor', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (5, 'Tevez', 42522206508, '485271250', '1994-11-13', 'M', 'Professor', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (6, 'Dudu', 42520650845, '485271250', '1994-11-13', 'f', 'cozinheira', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
+
+INSERT INTO cliente (idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf, Observacoes)
+VALUES (7, 'Richard Rios', 42522206508, '485271250', '1994-11-13', 'M', 'Professor', 'Brasileiro', 'Rua antonio cutiaro', '171', 'casa 2', 'tijuco preto', 'cotia', 'sp', NULL);
